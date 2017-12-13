5afb61a5e0cff66bb20294a41930a6fc9eedfd46
Zongqing Hou <zongqingh@gmail.com>
2017-12-13 21:40:45 +0800

    :(

--- alph.py
+++ alph.py
@@ -6,23 +6,22 @@ max_length = 6
 def score_func(value):
 	return 2 if value % 2 == 0 else -1
 
-
 def alph(k, t, prob):
 	score = 0
-	move_list = [0 for temp in range(k)]
+	move_list = {temp:[] for temp in range(k)}
 	for index in range(t):
 		if np.random.randn() < prob:
 			value = np.random.randint(1, k)
 		else:
-			value = np.argmax(move_list)
+			value = np.argmax([sum(move_list[temp]) / len(move_list[temp]) for temp in move_list if len(move_list[temp]) != 0])
 
 		temp = score_func(value)
 		print('you got {}'.format(temp))
+		move_list[value].append(temp)
 		score += temp
-		move_list[value] = (sum(move_list) + temp) / (len(move_list) + 1)
 	return score
 
 if __name__ == '__main__':
-	np.random.seed(16)
+	np.random.seed(1)
 	prob = np.random.randn()
 	print(alph(int(sys.argv[1]), int(sys.argv[2]), prob))
